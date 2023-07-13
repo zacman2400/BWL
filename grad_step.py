ed5a51d95b61957b4eafd54d592d1be0d0a5c37b
Zachary Miller <zmiller5000@gmail.com>
2023-07-12 21:03:40 -0500

    Update grad_step.py

--- grad_step.py
+++ grad_step.py
@@ -1,39 +1,63 @@
-def calculate_sense0a(M_t,ksp,mps_c,coord_t,dcf):
-        
-        F = sp.linop.NUFFT([mps_c.shape[0], mps_c.shape[1], mps_c.shape[2]], coord_t, oversamp=1.25, width=4)
-        F_torch = sp.to_pytorch_function(F, input_iscomplex=True, output_iscomplex=True)
-        FH_torch = sp.to_pytorch_function(F.H, input_iscomplex=True, output_iscomplex=True)
-        e_tc=F_torch.apply(M_t)
-        e_tca=torch.complex(e_tc[:,0],e_tc[:,1])
-     
-  
-       
-        resk=(e_tca-ksp)*dcf
-        resk_real=torch.real(torch.squeeze(resk))
-        resk_imag=torch.imag(torch.squeeze(resk))
+# Required packages
+import torch
+import scipy as sp
+
+def calculate_sense0a(input_image, kspace_data, sensitivity_map, coordinates, density_compensation):
+    """
+    This function calculates the sense of a given image using Non-uniform FFT and gradients.
     
-        resk_real=resk_real.unsqueeze(axis=1)
-        resk_imag=resk_imag.unsqueeze(axis=1)
-        resk_com=torch.cat([resk_real,resk_imag],axis=1)
-        g=torch.complex(g[:,:,:,0],g[:,:,:,1])*torch.conj(mps_c)
-        return g.cuda()
+    Parameters:
+    input_image: The input image data
+    kspace_data: The k-space data
+    sensitivity_map: The coil sensitivity map
+    coordinates: The coordinates for Non-uniform FFT
+    density_compensation: The density compensation factor
+    """
+        
+    # Compute Non-uniform FFT
+    non_uniform_fft = sp.linop.NUFFT([sensitivity_map.shape[0], sensitivity_map.shape[1], sensitivity_map.shape[2]], coordinates, oversamp=1.25, width=4)
+    nufft_torch = sp.to_pytorch_function(non_uniform_fft, input_iscomplex=True, output_iscomplex=True)
+    nufft_torch_adj = sp.to_pytorch_function(non_uniform_fft.H, input_iscomplex=True, output_iscomplex=True)
+    
+    encoded_image = nufft_torch.apply(input_image)
+    encoded_image_complex = torch.complex(encoded_image[:,0],encoded_image[:,1])
+    
+    # Compute residuals
+    residuals = (encoded_image_complex - kspace_data) * density_compensation
+    residuals_real = torch.real(torch.squeeze(residuals))
+    residuals_imag = torch.imag(torch.squeeze(residuals))
+    
+    residuals_real = residuals_real.unsqueeze(axis=1)
+    residuals_imag = residuals_imag.unsqueeze(axis=1)
+    residuals_complex = torch.cat([residuals_real, residuals_imag], axis=1)
+    
+    gradient = torch.complex(residuals_complex[:,:,:,0], residuals_complex[:,:,:,1]) * torch.conj(sensitivity_map)
+    
+    return gradient.cuda()
 
-def grad_step(img_t,ksp_t,dcf_t,coord_t,mps,alpha): #ima,deform_adjoint1,ksp,coord,dcf,mps,t,device,tr_per_frame):
- 
+def gradient_step(image, kspace, density_compensation, coordinates, sensitivity_map, learning_rate): 
+    """
+    This function performs a gradient step in the image reconstruction process.
+    
+    Parameters:
+    image: The input image data
+    kspace: The k-space data
+    density_compensation: The density compensation factor
+    coordinates: The coordinates for Non-uniform FFT
+    sensitivity_map: The coil sensitivity map
+    learning_rate: The learning rate for the gradient step
+    """
+    # Initialize gradient
+    gradient = 0
+    for coil in range(sensitivity_map.shape[0]):
+        reshaped_real_image = torch.reshape(torch.real(image*sensitivity_map[coil].cuda()),[sensitivity_map.shape[1], sensitivity_map.shape[2], sensitivity_map.shape[3],1])
+        reshaped_imag_image = torch.reshape(torch.imag(image*sensitivity_map[coil].cuda()),[sensitivity_map.shape[1], sensitivity_map.shape[2], sensitivity_map.shape[3],1])
+        concatenated_image = torch.cat([reshaped_real_image, reshaped_imag_image], axis=3)
 
-# Data consistency.
+        gradient += torch.utils.checkpoint.checkpoint(calculate_sense0a, concatenated_image, torch.reshape(kspace[coil],[-1]), sensitivity_map[coil].cuda(), torch.reshape(coordinates,[-1,3]), torch.reshape(density_compensation,[-1]))
 
-  g=0
-  for c in range(mps.shape[0]):
- 
- 
+    # Update image using gradient
+    image = image - learning_rate * gradient
 
-    g=g+torch.utils.checkpoint.checkpoint(calculate_sense0a,torch.cat([torch.reshape(torch.real(img_t*mps[c].cuda()),[mps.shape[1],mps.shape[2],mps.shape[3],1]),torch.reshape(torch.imag(img_t*mps[c].cuda()),[mps.shape[1],mps.shape[2],mps.shape[3],1])],axis=3),torch.reshape(ksp_t[c],[-1]),mps[c].cuda(),torch.reshape(coord_t,[-1,3]),torch.reshape(dcf_t,[-1]))
-  img_t=img_t-alpha*g
- 
+    return image
 
-
-
-
-
-  return img_t
